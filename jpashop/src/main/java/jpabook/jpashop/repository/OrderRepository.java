14a8797a45ebd4deeb58250f547bb6a4fdedaa75
hangyeol0531 <jhan0531@naver.com>
2023-09-24 17:45:51 +0900

    querydsl

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -9,11 +12,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
 
     public void save(Order order) {
         em.persist(order);
@@ -23,14 +35,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAllOld(OrderSearch orderSearch) {
         return em.createQuery("select o from Order o join o.member m" +
-                        " where o.status = :status " +
-                        " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000)
-                .getResultList();
+                " where o.status = :status " +
+                " and m.name like :name", Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+            .setMaxResults(1000)
+            .getResultList();
     }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
@@ -61,7 +73,7 @@ public class OrderRepository {
         }
 
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                .setMaxResults(1000);
+            .setMaxResults(1000);
 
         if (orderSearch.getOrderStatus() != null) {
             query = query.setParameter("status", orderSearch.getOrderStatus());
@@ -73,14 +85,42 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return queryFactory.select(order)
+            .from(order)
+            .join(order.member, member)
+            .where(
+                statusEq(orderSearch.getOrderStatus()),
+                nameLike(orderSearch.getMemberName())
+            )
+            .limit(1000)
+            .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus status) {
+        // null return where에서 적용안함
+        if (status == null) {
+            return null;
+        }
+        return order.status.eq(status);
+    }
+
     /**
      * DTO 조회 형식보다 성능이 안좋음, 재사용성이 높음
      */
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
-                "select o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class
+            "select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class
         ).getResultList();
     }
 
@@ -89,12 +129,12 @@ public class OrderRepository {
      */
     public List<Order> findAllWithItem() {
         return em.createQuery(
-            "select distinct o from Order o" +
+                "select distinct o from Order o" +
                     " join fetch o.member m" +
                     " join fetch o.delivery d" +
                     " join fetch o.orderItems oi" +
                     " join fetch oi.item i", Order.class)
-                .getResultList();
+            .getResultList();
     }
 
     /**
@@ -105,10 +145,10 @@ public class OrderRepository {
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery(
                 "select o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class
-                ).setFirstResult(offset)
-                .setMaxResults(limit)
-                .getResultList();
+                    " join fetch o.member m" +
+                    " join fetch o.delivery d", Order.class
+            ).setFirstResult(offset)
+            .setMaxResults(limit)
+            .getResultList();
     }
 }
